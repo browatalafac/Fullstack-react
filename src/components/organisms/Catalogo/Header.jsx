c735526b039a37ede6c132a0d92c684f2166ff0f
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2025-10-02 13:16:05 -0300

    Se anadio catalogo pero hace falta plicarle su propio estilo css

---
+++ src/components/organisms/Catalogo/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <h1>Catálogo de Productos</h1>
+        <p>Descubre nuestras tortas y postres más deliciosos</p>
+    </header>
+  )
+}
--- src/components/pages/Catalog.jsx
+++ src/components/pages/Catalog.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import TopBar from '../organisms/Catalogo/TopBar'
+import First from '../organisms/Catalogo/First'
+import Header from '../organisms/Catalogo/Header'
+import Container from '../organisms/Catalogo/Container'
 
 export default function Catalog() {
   return (
-    <div>Catalog</div>
+    <>
+      <TopBar/>
+      <First/>
+      <Header/>
+      <Container/>
+    </>
   )
 }
