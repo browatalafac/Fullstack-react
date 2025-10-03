86bda347cf5aca2f97f5a6013833654cf9801e85
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2025-10-03 11:00:09 -0300

    NO se cambio nada

--- src/components/organisms/Inicio/TopBar.jsx
+++ src/components/organisms/Inicio/TopBar.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
 
 export default function TopBar() {
   return (
     <div id="top-bar">
-      <p>Teléfono: +56 9 1567 9890</p>
-      <div className="social-icons">
-        <Link to="#"><i className="fab fa-instagram"></i></Link>
-        <Link to="#"><i className="fab fa-facebook"></i></Link>
-      </div>
+    <p>Teléfono: +56 9 1567 9890</p>
+        <div class="social-icons">
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-facebook"></i></a>
+        </div>
     </div>
-  );
+  )
 }
