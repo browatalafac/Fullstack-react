97b4eb389da2e9709397f86106e873120c1cec1c
browa <to.poncez@duocuc.cl>
2025-10-18 15:38:29 -0300

    perfil terminado

--- src/components/organisms/Profile/Card1.jsx
+++ src/components/organisms/Profile/Card1.jsx
@@ -6,8 +6,8 @@ export default function Card1() {
       <img className="photo" src="https://randomuser.me/api/portraits/men/32.jpg" alt="Foto de perfil" />
       <div className="info">
         <div><span className="label">Nombre Completo :</span> Tomas Ponce</div>
-        <div><span className="label">Teléfono :</span> +56 9 4222 8829</div>
-        <div><span className="label">Correo Electrónico :</span> to.poncez@duocuc.cl</div>
+        <div><span className="label">Teléfono :</span> +56 9 4432 8342</div>
+        <div><span className="label">Correo Electrónico :</span> pepe.pe@duocuc.cl</div>
         <div><span className="label">Direccion :</span>Av. Concha y Toro 3127</div>
       </div>
     </div>
--- src/components/organisms/Profile/Card2.jsx
+++ src/components/organisms/Profile/Card2.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export default function Card2() {
   return (
     <div className="changeInfo">
-        <div className="row">
+      <h2 className="changeInfo-title">Editar información del perfil</h2>
+      <div className="row">
         <label htmlFor="nombre">Nombre</label>
-        <input type="text" id="nombre" placeholder="cambiar nombre"/>
+        <input type="text" id="nombre" placeholder="Cambia tu nombre" />
       </div>
-        <div className="row">
+      <div className="row">
         <label htmlFor="apellido">Apellido</label>
-        <input type="text" id="apellido" placeholder="cambiar apellido"/>
-      </div>
-        <div className="row">
-        <label htmlFor="email">Correo</label>
-        <input type="text" id="correo" placeholder="cambiar correo"/>
-       </div>
-      <div className="row">
-        <label htmlFor="adress">Dirección</label>
-        <input type="text" id="direccion" placeholder="cambiar dirección"/>
+        <input type="text" id="apellido" placeholder="Cambia tu apellido" />
       </div>
       <div className="row">
-        <label htmlFor="contrase{a">Contraseña</label>
-        <input type="text" id="contraseña" placeholder="cambiar contraseña"/>
+        <label htmlFor="correo">Correo</label>
+        <input type="text" id="correo" placeholder="Cambia tu correo" />
       </div>
+      <div className="row">
+        <label htmlFor="direccion">Dirección</label>
+        <input type="text" id="direccion" placeholder="Cambia tu dirección" />
+      </div>
+      <div className="row">
+        <label htmlFor="contraseña">Contraseña</label>
+        <input type="password" id="contraseña" placeholder="Cambia tu contraseña" />
+      </div>
+      <button className="save-btn">Guardar</button>
     </div>
-  )
+  );
 }
