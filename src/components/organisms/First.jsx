559d22d18523547d688d8fc870edb75cebbb1846
browa <to.poncez@duocuc.cl>
2025-11-15 12:56:46 -0300

    first, footer y topbar ahora tienen su css propio

--- src/components/organisms/First.jsx
+++ src/components/organisms/First.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "./First.css";
 
 export default function First() {
   const navigate = useNavigate();
--- src/components/organisms/TopBar.jsx
+++ src/components/organisms/TopBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './TopBar.css'
 
 export default function TopBar() {
   return (
