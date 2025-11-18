0ce9611f2565731f3fed33ded3f19b774bea23ba
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2025-11-18 15:53:07 -0300

    caca

--- src/components/organisms/TopBar.jsx
+++ src/components/organisms/TopBar.jsx
@@ -4,7 +4,7 @@ import './TopBar.css'
 export default function TopBar() {
   return (
     <div id="top-bar">
-    <p>Teléfono: +56 9 1567 9890</p>
+    <p>Teléfono: +56940754456</p>
         <div className="social-icons">
             <a href="#"><i className="fab fa-instagram"></i></a>
             <a href="#"><i className="fab fa-facebook"></i></a>
